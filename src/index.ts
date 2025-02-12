2cf3a346a8af381c584835ff0a460c94c8ed0302
majo4957 <wraikey@gmail.com>
2025-02-12 10:10:07 +0100

    beginning

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import { Hono } from "hono";
+
+const app = new Hono();
+
+app.get("/stateless-add", (c) => {
+  const x = +(c.req.query("x") || 0);
+  if (isNaN(x)) {
+    return c.text("Invalid x");
+  }
+  const y = +(c.req.query("y") || 0);
+  if (isNaN(y)) {
+    return c.text("Invalid y");
+  }
+
+  const result = x + y;
+  return c.json({ result });
+});
+
+let state = 0;
+
+app.get("/add", (c) => {
+  const y = +(c.req.query("y") || 0);
+  if (isNaN(y)) {
+    return c.text("Invalid y");
+  }
+
+  state += 2;
+  const result = state;
+  return c.json({ result });
+});
+
+app.get("/", (c) => {
+  return c.text("Hello Homo!");
+});
+
+export default app;
